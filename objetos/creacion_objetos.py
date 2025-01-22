e246b4365b0862d44d251b9a0fdb29c9eb63822f
conradoTT <conrado.mendez@tripleten-team.com>
2025-01-22 18:00:05 -0300

    ejemplo clase y objetos en memoria

---
+++ objetos/creacion_objetos.py
@@ -0,0 +1,14 @@
+class User():
+    is_registered = True
+    name = "Mark"
+    login_user = "mark@gmail.com"
+
+
+user_1 = User()
+user_2 = User()
+user_3 = User()
+
+#Imprimiendo objetos
+print(user_3)
+print(user_2)
+print(user_1)
