132c4b3d204a975ccb43f7e3dcca7d57aa8179dd
conradoTT <conrado.mendez@tripleten-team.com>
2025-01-22 16:56:35 -0300

    fixing

--- atributos/atributos.py
+++ atributos/atributos.py
@@ -4,12 +4,16 @@ class User():
     login_user = "mark@gmail.com"
 
 
-print(User.is_registered)
-print(User.name)
+print(f"Registrado --- {User.is_registered}")
+print(f"Nombre del usuario --- {User.name}")
+print(f"Email de usuario --- {User.login_user}")
 
 #Cambio algun atributo
 User.name = "Conrado"
 
 #Imprimo lo mismo que arriba
-print(User.is_registered)
-print(f"El nuevo nombre asignado es: {User.name}")
+print("CAMBIANDO LOS ATRIBUTOS")
+
+print(f"Registrado --- {User.is_registered}")
+print(f"Nombre del usuario --- {User.name}")
+print(f"Email de usuario --- {User.login_user}")
